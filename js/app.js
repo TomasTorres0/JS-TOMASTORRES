89fd947cb1e65dac4949b4e9f7a0aa5063997060
Tomas Torres <axelagustom07@gmail.com>
2022-07-10 18:38:58 -0300

    aplicando javascript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 console.log("Hola consola , A&T")
 prompt("Escribe tu Nombre")
-alert("El arte de la programacion es la habilidad de controlar la complejidad.Un gran programa es moderado, hecho simple en su complejidad")
+alert("El arte de la programacion es la habilidad de controlar la complejidad. Un gran programa es moderado, hecho simple en su complejidad")
